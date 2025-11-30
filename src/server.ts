17063434c6d48bb87143512ac77f2c3731c5cd47
asraful-07 <asraful0508@gmail.com>
2025-11-30 17:14:19 +0600

    add full crud api

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import path from "path";
 const app = express();
 app.use(express.json());
 dotenv.config({ path: path.join(process.cwd(), ".env") });
+const PORT = process.env.PORT;
 
 //TODO database setup
 const pool = new Pool({
@@ -40,7 +41,7 @@ const intoDB = async () => {
 
 intoDB();
 
-//* POST api
+//* POST users
 app.post("/users", async (req: Request, res: Response) => {
   const { name, email, address } = req.body;
 
@@ -59,8 +60,8 @@ app.post("/users", async (req: Request, res: Response) => {
       data: result.rows[0],
       message: "Data insert done",
     });
-  } catch (error) {
-    console.error(error);
+  } catch (err: any) {
+    console.error(err);
     res.status(500).json({
       success: false,
       message: "Internal server error",
@@ -68,11 +69,108 @@ app.post("/users", async (req: Request, res: Response) => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+//* GET users
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const users = await pool.query("SELECT * FROM users");
+
+    res.status(200).json({
+      success: true,
+      data: users.rows,
+      message: "Users data fetched successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
 });
 
-const PORT = process.env.PORT;
+//* GET single users
+app.get("/user/:id", async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid user id",
+      });
+    }
+    //? [req.params.id]
+    const user = await pool.query(`SELECT * FROM users WHERE id = $1`, [id]);
+
+    if (!user.rows.length) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: user.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+});
+
+//* PUT users update
+app.put("/user/:id", async (req: Request, res: Response) => {
+  const { name, email, address } = req.body;
+  try {
+    const user = await pool.query(
+      `UPDATE users 
+       SET name=$1, email=$2, address=$3 
+       WHERE id=$4 
+       RETURNING *`,
+      [name, email, address, req.params.id]
+    );
+
+    if (!user.rows.length) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: user.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+});
+
+//* DELETE users delete
+app.delete("/user/:id", async (req: Request, res: Response) => {
+  try {
+    const user = await pool.query(
+      `DELETE FROM users WHERE id = $1 RETURNING *`,
+      [req.params.id]
+    );
+
+    if (!user.rows.length) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    res.status(200).json({ success: true, message: "Deleted successfully" });
+  } catch (err: any) {
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
